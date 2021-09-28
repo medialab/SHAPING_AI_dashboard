48b7f9072fa67106fd2cad6cb90f40f0955cd5b6
Yuliia Nikolaenko <51966068+yuliianikolaenko@users.noreply.github.com>
2021-09-28 10:50:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -120,7 +120,6 @@ elif choice == 'Analysis':
 elif choice == 'Topics':
     st.title("Topic Modeling")
     st.info('Topics were extracted from the text corpus using the Latent Dirichlet Allocation (LDA) model with Scikit-learn open-source Python machine learning library. The number of topics was selected manually through the comparison and selection of the highest Topic Coherence score.')
-    st.title("Top words discussed in each topic")
     st.subheader('Choose Topic')
     option_2_s = st.selectbox('Topic', ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
     col1, col2 = st.columns(2)
