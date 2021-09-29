cf04ba073a1d6725438576409f202bad13bad260
Yuliia Nikolaenko <51966068+yuliianikolaenko@users.noreply.github.com>
2021-09-29 16:58:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -100,8 +100,11 @@ elif choice == 'Analysis':
     max_ts = max(dist_articles_df['date']).to_pydatetime()
     min_selection, max_selection = pd.to_datetime(st.slider("", min_value=min_ts, max_value=max_ts, value=[min_ts, max_ts]))
     dist_articles_df = dist_articles_df[(dist_articles_df["date"] >= min_selection) & (dist_articles_df["date"] <= max_selection)]
-    st.subheader('Articles distribution over time')
-    st.plotly_chart(draw_dist(dist_articles_df))
+    my_expander = st.expander()
+    my_expander.write('Hello there!')
+    clicked = my_expander.button('Click me!')
+    my_expander.subheader('Articles distribution over time')
+    my_expander.plotly_chart(draw_dist(dist_articles_df))
     col1, col2 = st.columns(2)
     col1.subheader('Most frequent words')
     data = load_bigram(min_selection, max_selection)
