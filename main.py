d9ce79b9dd4a5fdc50c1960834b0d80384565cfe
Yuliia Nikolaenko <51966068+yuliianikolaenko@users.noreply.github.com>
2021-09-29 18:07:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -100,6 +100,7 @@ elif choice == 'Analysis':
     max_ts = max(dist_articles_df['date']).to_pydatetime()
     min_selection, max_selection = pd.to_datetime(st.slider("", min_value=min_ts, max_value=max_ts, value=[min_ts, max_ts]))
     dist_articles_df = dist_articles_df[(dist_articles_df["date"] >= min_selection) & (dist_articles_df["date"] <= max_selection)]
+    st.expander('Expander')
     st.subheader('Articles distribution over time')
     st.plotly_chart(draw_dist(dist_articles_df))
     col1, col2 = st.columns(2)
